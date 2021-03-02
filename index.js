1750bbb1c26d12b708b5f1381414aff6a3a790e4
Nicholas V Frangione <nfrangione8@gmail.com>
2021-03-02 11:57:50 -0800

    commit tree 74631ffc679614b437d9f54f00ac91267419bd7c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+let submitData = (nameInput, emailInput) => {
+    let formData = {
+        name: nameInput,
+        email: emailInput
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let p = document.createElement('p');
+            p.innerHTML = object.id;
+            document.body.appendChild(p);
+        })
+        .catch(function(error) {
+            let p = document.createElement('p');
+            p.innerHTML = error.message;
+            document.body.appendChild(p);
+        });
+};
+
+  
